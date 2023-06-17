941320d27b8d22ac59dcecadba5bb41ef55d35ef
Rifki Perdiansyah <rifkiperdiansyah99@gmail.com>
2023-06-17 00:43:12 -0700

    Network Sepeda motor

--- app/src/main/java/id/ac/unpas/showroommobile8/networks/SepedaMotorSingleGetResponse.kt
+++ app/src/main/java/id/ac/unpas/showroommobile8/networks/SepedaMotorSingleGetResponse.kt
@@ -2,6 +2,7 @@ package id.ac.unpas.showroommobile8.networks
 
 import id.ac.unpas.showroommobile8.model.SepedaMotor
 
+
 data class SepedaMotorSingleGetResponse(
     val data: SepedaMotor? = null
 )
